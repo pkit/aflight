9ada5492ba02458013124aac98361a68e60f07ee
Constantine Peresypkin <constantine@caspiandb.com>
2023-03-23 17:31:23 +0100

    v0.2.2
    add argument to exec

--- aflight/exec.py
+++ aflight/exec.py
@@ -1,6 +1,9 @@
+import sys
+
 from aflight.__main__ import Cli
 
 if __name__ == "__main__":
     cli = Cli()
     app = cli.load_app()
-    app.execute()
+    cmd, = sys.argv[1:2] or ['']
+    app.execute(command=cmd.encode("utf-8"))
